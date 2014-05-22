705cad447387eadd63c1800ec0b1172821620eca
Nick Levantis <nicklevantis@gmail.com>
2014-05-22 17:54:06 +0300

    dsfs

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ $(window).load(function() { // makes sure the whole site is loaded
 	
 	
 	$("#status").fadeOut(); // will first fade out the loading animation
-	$("#preloader").delay(350).fadeOut("slow"); // will fade out the white DIV that covers the website.
+	$("#preloader").delay(350).fadeOut("slow");
 
 
 
